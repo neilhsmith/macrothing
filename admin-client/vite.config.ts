323e739ea858961848d35f66557ff33dbfa1789c
Neil Smith <me+github@neilsmith.fyi>
2023-12-30 14:02:46 -0600

    seup dev ssl

--- admin-client/vite.config.ts
+++ admin-client/vite.config.ts
@@ -1,9 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react-swc"
+import mkcert from "vite-plugin-mkcert"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
+  server: {
+    port: 3060,
+    // https: {
+
+    // }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
