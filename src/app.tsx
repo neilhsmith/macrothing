315c7915fd7dddf49b4751c4bf61367986ab293d
Neil Smith <me+github@neilsmith.fyi>
2023-12-30 00:57:13 -0600

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+export function App() {
+  return (
+    <div className="h-screen bg-background text-foreground">
+      <p>asdfsfsdf</p>
+      <Button>idk</Button>
+      <Card className="w-[250px]">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
